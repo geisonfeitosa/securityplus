a201ec070523e35a0dc3624e640a097ff3a8fef1
Fazenda <Fazenda@FAZENDA05>
2019-10-02 12:03:06 -0300

    teste

---
+++ src/main/java/br/com/securityplus/securityplus/SecurityplusApplication.java
@@ -0,0 +1,13 @@
+package br.com.securityplus.securityplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityplusApplication.class, args);
+	}
+
+}
